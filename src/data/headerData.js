a55f05dce3d618a1aacc21005c896f0edb61efd7
Ks000007 <shreyashkars172@gmail.com>
2023-09-10 10:38:51 +0530

    first-commit

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Ashutosh Yadav',
+    title: "Data Scientist",
+    desciption: "Welcome to a world where data transforms possibilities into reality. I am a seasoned data scientist, driven by a relentless pursuit of insights hidden within the numbers. With a blend of analytical rigor and creative ingenuity, I unlock the power of data to drive informed decisions and fuel innovation.",
+    image: 'https://c.saavncdn.com/artists/Paresh_Rawal_001_20230605094525_500x500.jpg',
+    resumePdf: resume
+}
